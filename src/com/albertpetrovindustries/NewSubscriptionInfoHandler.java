24a22dd6aa9066f56b30ac07ea5fd6fbb7e94f5d
AlbertPetrov <46540467+AlbertPetrov@users.noreply.github.com>
2020-05-27 18:58:42 +0300

    Update NewSubscriptionInfoHandler.java

--- src/com/albertpetrovindustries/NewSubscriptionInfoHandler.java
+++ src/com/albertpetrovindustries/NewSubscriptionInfoHandler.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 /**
  *  @author Christinica
- *  @abstract class NewSubscriptionInfoHandler and his subclasses is a chain of responsibility pattern
+ *  abstract class NewSubscriptionInfoHandler and his subclasses is a chain of responsibility pattern
  */
 public abstract class NewSubscriptionInfoHandler {
     
     /**
-     * @field newSubscriptionInfoHandler invokes {@link handle} methods of implementations 
+     * @ield newSubscriptionInfoHandler invokes {@link handle} methods of implementations 
      *   of abstract class NewSubscriptionInfoHandler
      */
 	public NewSubscriptionInfoHandler newSubscriptionInfoHandler;
 	
 	/**
-	 * @contractor NewSubscriptionInfoHandler takes an implementation of this abstract class
+	 * contractor NewSubscriptionInfoHandler takes an implementation of this abstract class
 	 *   from constructors of implemented classes
 	 * @param newSubscriptionInfoHandler
 	 */
@@ -24,7 +24,7 @@ public abstract class NewSubscriptionInfoHandler {
 	}
 
 	/**
-	 * @method handle - process our new line with scanner
+	 * method handle - process our new line with scanner
 	 * @param line gets a line of a string after scanner using
 	 * @param line is a line from an old line
 	 * @return returns a processed string and send it to other handler 
